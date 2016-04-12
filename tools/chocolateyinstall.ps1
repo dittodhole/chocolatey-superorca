97e7d07ba900eabaac08b77e48e40e1a82e457d3
Andreas Niedermair <andreas@niedermair.name>
2016-04-12 17:05:21 +0200

    added basic installation

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$packageName = 'superorca'
+$installerType = 'exe'
+$silentArgs = '/SILENT /NORESTART /CLOSEAPPLICATIONS /RESTARTAPPLICATIONS'
+$url = 'http://www.pantaray.com/SuperOrcaSetup.exe'
+$checksum = 'c2751e72bd5a8593f4eb4ebd000ace791db0a9c7'
+$checksumType = 'sha1'
+$validExitCodes = @(0)
+
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
---
+++ tools/chocolateyuninstall.ps1
@@ -0,0 +1,19 @@
+﻿$packageName = 'superorca'
+$packageSearch = 'superorca'
+$installerType = 'exe'
+$silentArgs = '/SILENT'
+
+try {
+  Get-ItemProperty -Path @( 'HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                            'HKLM:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*',
+                            'HKCU:\Software\Microsoft\Windows\CurrentVersion\Uninstall\*' ) `
+                   -ErrorAction:SilentlyContinue `
+  | Where-Object   { $_.DisplayName -like "$packageSearch*" } `
+  | ForEach-Object { Uninstall-ChocolateyPackage -PackageName "$packageName" `
+                                                 -FileType "$installerType" `
+                                                 -SilentArgs "$($_.PSChildName) $silentArgs" `
+                                                 -File $_.UninstallString }
+} catch {
+  Write-ChocolateyFailure $packageName $($_.Exception.Message)
+  throw
+}
